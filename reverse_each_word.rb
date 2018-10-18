9199e92dd9756aec39370f1cf637b74f7f7b789f
beaulieudev <beaulieudev@gmail.com>
2018-10-18 00:01:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 
  def reverse_each_word(string)
-   string.each do |string|
+   string.each do |arr|
       arr = string.split(" ")
       new_arr = arr.collect{|a| a.reverse}
       new_arr.join(" ")
